53da061a0cc48939baa23c342d78db4734a42103
Krish Palaniappan <krish@getsnowpal.com>
2023-10-18 16:29:39 -0400

    feat: added more endpoints (#4)
    * feat: added initial setup files
    
    * feat: added routes + field types endpoint
    
    * feat: added create text attribute endpoint
    
    ---------
    
    Co-authored-by: Anish Ghumara <khatri.anis@gmail.com>

---
+++ lib/endpoints/custom_attributes.1.create_attributes/custom_attributes.1.1.create_text_attribute.go
@@ -0,0 +1,62 @@
+package custom_attributes
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/request"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/response"
+)
+
+func CreateTextAttribute(jwtToken string, reqBody request.TextAttrReq) (response.TextAttr, error) {
+	var resTextAttr response.TextAttr
+
+	payload, err := helpers.GetRequestPayload(reqBody)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+
+	var route string
+	route, err = helpers.GetRoute(lib.RouteAttributesCreateTextAttribute)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPost, route, payload)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+
+	err = json.Unmarshal(body, &resTextAttr)
+	if err != nil {
+		fmt.Println(err)
+		return resTextAttr, err
+	}
+	return resTextAttr, nil
+}
---
+++ lib/endpoints/field_types/field_types.1.1.get_field_types.go
@@ -0,0 +1,53 @@
+package field_types
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/response"
+)
+
+func GetFieldTypes(jwtToken string) ([]response.FieldType, error) {
+	resFieldTypes := response.FieldTypes{}
+	route, err := helpers.GetRoute(lib.RouteFieldTypesGetFieldTypes)
+	if err != nil {
+		fmt.Println(err)
+		return resFieldTypes.FieldTypes, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodGet, route, nil)
+	if err != nil {
+		fmt.Println(err)
+		return resFieldTypes.FieldTypes, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return resFieldTypes.FieldTypes, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return resFieldTypes.FieldTypes, err
+	}
+
+	err = json.Unmarshal(body, &resFieldTypes)
+	if err != nil {
+		fmt.Println(err)
+		return resFieldTypes.FieldTypes, err
+	}
+	return resFieldTypes.FieldTypes, nil
+}
