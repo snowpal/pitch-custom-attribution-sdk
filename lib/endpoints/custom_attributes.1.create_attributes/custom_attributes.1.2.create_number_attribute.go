8f51cf01a19e5d20ac4f4331fef663dff9d736c4
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-19 18:56:42 -0400

    Create custom_attributes.1.2.create_number_attribute.go (#8)

---
+++ lib/endpoints/custom_attributes.1.create_attributes/custom_attributes.1.2.create_number_attribute.go
@@ -0,0 +1,60 @@
+package custom_attributes
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+)
+
+func CreateNumberAttribute(jwtToken string, reqBody any) (any, error) {
+	var resNumberAttr any
+
+	payload, err := helpers.GetRequestPayload(reqBody)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+
+	var route string
+	route, err = helpers.GetRoute(lib.RouteAttributesCreateTextAttribute)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPost, route, payload)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+
+	err = json.Unmarshal(body, &resNumberAttr)
+	if err != nil {
+		fmt.Println(err)
+		return resNumberAttr, err
+	}
+	return resNumberAttr, nil
+}
