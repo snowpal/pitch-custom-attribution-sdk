50ad91b7fd4323da2a6afc76f3fb560414d1c462
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-20 14:53:10 -0400

    feat: added `CustomAttribute.1.7: Create Attribute (Nested: Single Select)` endpoint

---
+++ lib/endpoints/custom_attributes.1.create_attributes/custom_attributes.1.7.create_nested_single_select_attribute.go
@@ -0,0 +1,60 @@
+package custom_attributes
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+)
+
+func CreateNestedSingleSelectAttribute(jwtToken string, reqBody any) (any, error) {
+	var resNestedSingleSelectAttr any
+
+	payload, err := helpers.GetRequestPayload(reqBody)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+
+	var route string
+	route, err = helpers.GetRoute(lib.RouteAttributesCreateNestedSingleSelectAttribute)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPost, route, payload)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+
+	err = json.Unmarshal(body, &resNestedSingleSelectAttr)
+	if err != nil {
+		fmt.Println(err)
+		return resNestedSingleSelectAttr, err
+	}
+	return resNestedSingleSelectAttr, nil
+}
