79ddc41de4aa589c4798b1ca5f47067f0f51834c
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-23 18:17:29 -0400

    feat: added `Resources.1.1: Associate attribute bags to resource` endpoint

---
+++ lib/endpoints/resources/resources.1.1.associate_attribute_bags_to_resource.go
@@ -0,0 +1,59 @@
+package resources
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"io"
+	"net/http"
+)
+
+func AssociateAttrBagToResource(jwtToken string, reqBody any) (any, error) {
+	var resAttributeBag any
+
+	payload, err := helpers.GetRequestPayload(reqBody)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+
+	var route string
+	route, err = helpers.GetRoute(lib.RouteResourcesAssociateAttributeBagsToResource)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPut, route, payload)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+
+	err = json.Unmarshal(body, &resAttributeBag)
+	if err != nil {
+		fmt.Println(err)
+		return resAttributeBag, err
+	}
+	return resAttributeBag, nil
+}
