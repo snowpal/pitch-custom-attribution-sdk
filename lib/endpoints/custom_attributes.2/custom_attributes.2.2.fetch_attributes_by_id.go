1cb1748728ac475a9305069a177255909f52ff87
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-23 14:52:15 -0400

    feat: added `2.2: Fetch attribute by ID` endpoint

---
+++ lib/endpoints/custom_attributes.2/custom_attributes.2.2.fetch_attributes_by_id.go
@@ -0,0 +1,52 @@
+package custom_attributes_2
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+)
+
+func FetchAttributesByID(jwtToken string) (any, error) {
+	var resAttributesByID any
+	route, err := helpers.GetRoute(lib.RouteAttributesGetAttributeByID)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodGet, route, nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	var res *http.Response
+	res, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer helpers.CloseBody(res.Body)
+
+	var body []byte
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &resAttributesByID)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return nil, nil
+}
