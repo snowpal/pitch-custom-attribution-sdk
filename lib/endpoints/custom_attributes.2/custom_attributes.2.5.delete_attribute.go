28db841831b6e1009daa4adf157d1c842a7a1c4d
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-23 15:28:45 -0400

    feat: added `2.5: Delete attribute` endpoint

---
+++ lib/endpoints/custom_attributes.2/custom_attributes.2.5.delete_attribute.go
@@ -0,0 +1,33 @@
+package custom_attributes_2
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+)
+
+func DeleteAttribute(jwtToken string) error {
+	route, err := helpers.GetRoute(lib.RouteAttributesDeleteAttribute)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodPatch, route, nil)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	_, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
