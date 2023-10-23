27125dbdd4d42f28ee7c478c7d5db09afc0dd2ee
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-23 18:05:08 -0400

    feat: added `1.6: Delete bag` endpoint

---
+++ lib/endpoints/attributes_bag.1/attribute_bags.1.6.delete_attribute_bag.go
@@ -0,0 +1,32 @@
+package attributesbag
+
+import (
+	"fmt"
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"net/http"
+)
+
+func DeleteAttributeBag(jwtToken string) error {
+	route, err := helpers.GetRoute(lib.RouteAttributeBagsDeleteAttributeBag)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest(http.MethodDelete, route, nil)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	helpers.AddUserHeaders(jwtToken, req)
+
+	_, err = helpers.MakeRequest(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
