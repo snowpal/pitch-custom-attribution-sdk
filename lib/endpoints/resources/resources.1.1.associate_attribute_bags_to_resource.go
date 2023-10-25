4cc4d29edc69299fb45dc3e625c596340dfb8c83
Varun Palaniappan <varun.snowpal@gmail.com>
2023-10-25 15:48:29 -0400

    feat: wired in struct for `1.1: Associate Attribute Bags to Resource` endpoint

--- lib/endpoints/resources/resources.1.1.associate_attribute_bags_to_resource.go
+++ lib/endpoints/resources/resources.1.1.associate_attribute_bags_to_resource.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"github.com/snowpal/pitch-go-status-sdk/lib"
 	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/request"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/response"
 	"io"
 	"net/http"
 )
 
-func AssociateAttrBagToResource(jwtToken string, reqBody any) (any, error) {
-	var resAttributeBag any
+func AssociateAttrBagToResource(jwtToken string, reqBody request.AttrBagsReq) (response.Resource, error) {
+	var resAttributeBag response.Resource
 
 	payload, err := helpers.GetRequestPayload(reqBody)
 	if err != nil {
