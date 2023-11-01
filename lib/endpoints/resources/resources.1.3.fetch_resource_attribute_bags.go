251682132dbbfcd43169b29eb82b1886d827940e
Varun Palaniappan <varun.snowpal@gmail.com>
2023-11-01 14:24:58 -0400

    feat: wired in structs for `1.2 + 1.3` endpoints

--- lib/endpoints/resources/resources.1.3.fetch_resource_attribute_bags.go
+++ lib/endpoints/resources/resources.1.3.fetch_resource_attribute_bags.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"github.com/snowpal/pitch-go-status-sdk/lib"
 	"github.com/snowpal/pitch-go-status-sdk/lib/helpers"
+	"github.com/snowpal/pitch-go-status-sdk/lib/structs/response"
 	"io"
 	"net/http"
 )
 
-func FetchResourceAttrBags(jwtToken string) (any, error) {
-	var resAttributeBag any
+func FetchResourceAttrBags(jwtToken string) (response.AttrBags, error) {
+	var resAttributeBag response.AttrBags
 	route, err := helpers.GetRoute(lib.RouteResourcesGetResourceAttributeBags)
 	if err != nil {
 		fmt.Println(err)
