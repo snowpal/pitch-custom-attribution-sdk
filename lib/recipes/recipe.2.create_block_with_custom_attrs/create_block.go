9e16efd490f070746bbb6629ebb331559f21f51e
Anish Ghumara <khatri.anis@gmail.com>
2023-11-02 23:06:07 +0530

    feat: stub out methods + added create attrs (partial) (#18) (#19)

---
+++ lib/recipes/recipe.2.create_block_with_custom_attrs/create_block.go
@@ -0,0 +1,52 @@
+package blockattrs
+
+import (
+	"github.com/snowpal/pitch-go-status-sdk/lib"
+	"github.com/snowpal/pitch-go-status-sdk/lib/helpers/recipes"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func CreateBlockWithCustomAttrs() {
+	log.Info("Objective: Add a new team, add couple of members to that and report status for each 1 of them")
+	if _, err := recipes.ValidateDependencies(); err != nil {
+		return
+	}
+
+	user, err := recipes.SignIn(lib.ActiveUser, lib.Password)
+	if err != nil {
+		return
+	}
+
+	// Create all custom attributes you want to add to the block
+	attributeIDs, err := CreateBlockAttributes(user)
+	if err != nil {
+		return
+	}
+
+	// Create an attribute bag with the block attributes above
+	attributeBag, err := CreateAttributeBagForBlock(user, attributeIDs)
+	if err != nil {
+		return
+	}
+
+	var blockID primitive.ObjectID
+	// Associate the above attribute bag with a block
+	err = AssiciateBagWithBlock(user, blockID, attributeBag)
+	if err != nil {
+		return
+	}
+
+	// Add values to the block attributes
+	err = AddValuesToBlockAttributes(user, blockID)
+	if err != nil {
+		return
+	}
+
+	// Fetch all block custom attributes
+	err = FetchBlockAttributes(user)
+	if err != nil {
+		return
+	}
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/snowpal/pitch-go-status-sdk/lib/recipes"
 
 	log "github.com/sirupsen/logrus"
+	blockattrs "github.com/snowpal/pitch-go-status-sdk/lib/recipes/recipe.2.create_block_with_custom_attrs"
 )
 
 func main() {
@@ -19,6 +20,9 @@ func main() {
 	case 1:
 		log.Info("Run Recipe1")
 		recipes.RegisterFewUsers()
+	case 2:
+		log.Info("Run Recipe2")
+		blockattrs.CreateBlockWithCustomAttrs()
 	default:
 		log.Info("pick a specific recipe to run")
 	}
