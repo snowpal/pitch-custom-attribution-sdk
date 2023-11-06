ce4ec52c5db3ee1850442e64752f1eda8e0c57ab
Anish Ghumara <khatri.anis@gmail.com>
2023-11-06 21:34:46 +0530

    fix: fixed import issue

--- lib/recipes/recipe.2.create_block_with_custom_attrs/block_attrs.go
+++ lib/recipes/recipe.2.create_block_with_custom_attrs/block_attrs.go
@@ -2,9 +2,9 @@ package blockattrs
 
 import (
 	"github.com/snowpal/pitch-custom-attribution-sdk/lib/helpers/recipes"
+	"github.com/snowpal/pitch-custom-attribution-sdk/lib/structs/common"
 	"github.com/snowpal/pitch-custom-attribution-sdk/lib/structs/request"
-	"github.com/snowpal/pitch-go-status-sdk/lib/structs/common"
-	"github.com/snowpal/pitch-go-status-sdk/lib/structs/response"
+	"github.com/snowpal/pitch-custom-attribution-sdk/lib/structs/response"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	log "github.com/sirupsen/logrus"
